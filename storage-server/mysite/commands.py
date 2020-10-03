cf240287ad0d76cf51a81e895b7c73e62057df11
Danil Usmanov <usmanovdanilr@gmail.com>
2020-10-03 18:52:19 +0300

    + request naming server

--- storage-server/mysite/commands.py
+++ storage-server/mysite/commands.py
@@ -3,6 +3,7 @@ import os
 import shutil
 import time
 
+import requests
 from django.conf import settings
 from django.http import FileResponse, HttpResponse, JsonResponse
 
@@ -18,7 +19,21 @@ def file_create(name, cwd):
     if os.path.isfile(f"{settings.WORK_DIR}{cwd}{name}"):
         return JsonResponse({"msg": {"error": "file with such name already exists"}}, status=400)
     else:
-        open(f"{settings.WORK_DIR}{cwd}{name}", "w+")
+        open(f"{settings.WORK_DIR}{cwd}{name}", "w+")  # name host port cwd
+
+        r = requests.post(
+            f"{settings.HOST_NAMING}/api/file/approve/",
+            data={
+                "file": name,
+                "access time": time.ctime(os.path.getatime(f"{settings.WORK_DIR}{cwd}{name}")),
+                "modified time": time.ctime(os.path.getmtime(f"{settings.WORK_DIR}{cwd}{name}")),
+                "change time": time.ctime(os.path.getctime(f"{settings.WORK_DIR}{cwd}{name}")),
+                "size": os.path.getsize(f"{settings.WORK_DIR}{cwd}{name}"),
+            },
+            params={"name": name, "host": settings.HOST_IP, "port": settings.HOST_PORT, "cwd": cwd},
+        )
+        print(r.status_code)
+
         return HttpResponse(status=201)
 
 
@@ -29,19 +44,28 @@ def file_read(name, cwd):
         return FileResponse(open(f"{settings.WORK_DIR}{cwd}{name}", "rb"), as_attachment=True)
 
 
-def handle_uploaded_file(file, cwd):
-    with open(f"{settings.WORK_DIR}{cwd}{file.name}", "wb+") as dest:
-        for chunk in file.chunks():
-            dest.write(chunk)
-
-
 def file_write(request, cwd):
+    def handle_uploaded_file(file, cwd):
+        with open(f"{settings.WORK_DIR}{cwd}{file.name}", "wb+") as dest:
+            for chunk in file.chunks():
+                dest.write(chunk)
+
     file = request.FILES["file"]
-    # print(file.name)
-    # print(file.size)
-    # print(file.content_type)
-    # print(file.content_type_extra)
     handle_uploaded_file(file=file, cwd=cwd)
+
+    r = requests.post(
+        f"{settings.HOST_NAMING}/api/file/approve/",
+        data={
+            "file": file.name,
+            "access time": time.ctime(os.path.getatime(f"{settings.WORK_DIR}{cwd}{file.name}")),
+            "modified time": time.ctime(os.path.getmtime(f"{settings.WORK_DIR}{cwd}{file.name}")),
+            "change time": time.ctime(os.path.getctime(f"{settings.WORK_DIR}{cwd}{file.name}")),
+            "size": file.size,
+        },
+        params={"name": file.name, "host": settings.HOST_IP, "port": settings.HOST_PORT, "cwd": cwd},
+    )
+    print(r.status_code)
+
     return HttpResponse(status=200)
 
 
@@ -50,6 +74,12 @@ def file_delete(name, cwd):
         return JsonResponse({"msg": {"error": "file with such name does not exists"}}, status=404)
     else:
         os.remove(f"{settings.WORK_DIR}{cwd}{name}")
+        r = requests.post(
+            f"{settings.HOST_NAMING}/api/file/delete/",
+            data={},
+            params={"name": name, "host": settings.HOST_IP, "port": settings.HOST_PORT, "cwd": cwd},
+        )
+        print(r.status_code)
         return HttpResponse(status=200)
 
 
@@ -79,6 +109,18 @@ def file_copy(name, cwd):
         spltd_name = name.split(".")
         target = f"{spltd_name[0]}_{now}.{'.'.join(spltd_name[1::])}"
         shutil.copyfile(f"{settings.WORK_DIR}{cwd}{name}", f"{settings.WORK_DIR}{cwd}{target}")
+        r = requests.post(
+            f"{settings.HOST_NAMING}/api/file/approve/",
+            data={
+                "file": target,
+                "access time": time.ctime(os.path.getatime(f"{settings.WORK_DIR}{cwd}{target}")),
+                "modified time": time.ctime(os.path.getmtime(f"{settings.WORK_DIR}{cwd}{target}")),
+                "change time": time.ctime(os.path.getctime(f"{settings.WORK_DIR}{cwd}{target}")),
+                "size": os.path.getsize(f"{settings.WORK_DIR}{cwd}{target}"),
+            },
+            params={"name": target, "host": settings.HOST_IP, "port": settings.HOST_PORT, "cwd": cwd},
+        )
+        print(r.status_code)
         return JsonResponse({"msg": {"filename": target}}, status=404)
 
 
@@ -87,6 +129,23 @@ def file_move(name, cwd, path):
         return JsonResponse({"msg": {"error": "file with such name does not exists"}}, status=404)
     else:
         shutil.move(f"{settings.WORK_DIR}{cwd}{name}", f"{settings.WORK_DIR}{path}{name}")
+        r = requests.post(
+            f"{settings.HOST_NAMING}/api/file/delete/",
+            data={},
+            params={"name": name, "host": settings.HOST_IP, "port": settings.HOST_PORT, "cwd": cwd},
+        )
+        r = requests.post(
+            f"{settings.HOST_NAMING}/api/file/approve/",
+            data={
+                "file": name,
+                "access time": time.ctime(os.path.getatime(f"{settings.WORK_DIR}{path}{name}")),
+                "modified time": time.ctime(os.path.getmtime(f"{settings.WORK_DIR}{path}{name}")),
+                "change time": time.ctime(os.path.getctime(f"{settings.WORK_DIR}{path}{name}")),
+                "size": os.path.getsize(f"{settings.WORK_DIR}{path}{name}"),
+            },
+            params={"name": name, "host": settings.HOST_IP, "port": settings.HOST_PORT, "cwd": cwd},
+        )
+        print(r.status_code)
         return HttpResponse(status=200)
 
 
@@ -115,17 +174,29 @@ def dir_read(cwd):
         )
 
 
-def dir_make(name, cwd):
+def dir_make(name, cwd):  # egor
     if os.path.isdir(f"{settings.WORK_DIR}{cwd}"):
         return JsonResponse({"msg": {"error": "directory with such name already exists"}}, status=400)
     else:
         os.mkdir(f"{settings.WORK_DIR}{cwd}{name}")
+        r = requests.post(
+            f"{settings.HOST_NAMING}/api/file/approve/",
+            data={},
+            params={"name": name, "host": settings.HOST_IP, "port": settings.HOST_PORT, "cwd": cwd},
+        )
+        print(r.status_code)
         return HttpResponse(status=201)
 
 
-def dir_delete(cwd):
+def dir_delete(cwd):  # egor
     if not os.path.isdir(f"{settings.WORK_DIR}{cwd}"):
         return JsonResponse({"msg": {"error": "directory with such name already exists"}}, status=400)
     else:
         shutil.rmtree(f"{settings.WORK_DIR}{cwd}")
+        r = requests.post(
+            f"{settings.HOST_NAMING}/api/file/delete/",
+            data={},
+            params={"name": None, "host": settings.HOST_IP, "port": settings.HOST_PORT, "cwd": cwd},
+        )
+        print(r.status_code)
         return HttpResponse(status=200)
