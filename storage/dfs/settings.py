b2306d1d0d1d9ae2dccec263d59ac17a69922654
Danil Usmanov <usmanovdanilr@gmail.com>
2020-10-03 18:54:29 +0300

    ! storage folder

--- storage/dfs/settings.py
+++ storage/dfs/settings.py
@@ -15,7 +15,7 @@ import socket
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-WORK_DIR = BASE_DIR + "/storage"
+WORK_DIR = BASE_DIR + "/var"
 HOST_NAMING = "naming:80"
 HOST_IP = socket.gethostbyname(socket.gethostname())
 HOST_PORT = 8000
