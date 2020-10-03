235b0640393e3728315bde85f45b963baaafc6d0
Danil Usmanov <usmanovdanilr@gmail.com>
2020-10-03 18:51:32 +0300

    + consts

--- storage-server/dfs/settings.py
+++ storage-server/dfs/settings.py
@@ -11,11 +11,14 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+import socket
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 WORK_DIR = BASE_DIR + "/storage"
-
+HOST_NAMING = "naming:80"
+HOST_IP = socket.gethostbyname(socket.gethostname())
+HOST_PORT = 8000
 try:
     os.mkdir(WORK_DIR)
 except OSError:
