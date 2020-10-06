0e1e9e1f2384988af5e0dceed1a60dbe097108dc
Danil Usmanov <usmanovdanilr@gmail.com>
2020-10-06 21:26:54 +0300

    + recovery

--- storage/dfs/utils.py
+++ storage/dfs/utils.py
@@ -1,10 +1,13 @@
 import json
+import logging
 import os
 import shutil
 from random import choice
 
 import requests
 from django.conf import settings
+
+logger = logging.getLogger("common")
 
 
 def recovery(resp):
@@ -14,7 +17,6 @@ def recovery(resp):
         name = item.get("name", None)
         size = item.get("size", None)
         hosts = item.get("hosts", None)
-        # hosts = [d['value'] for d in l]
 
         if size is not None:  # if this is a file
             if os.path.isfile(f"{settings.WORK_DIR}{name}"):
@@ -31,7 +33,11 @@ def recovery(resp):
                         fp.write(r.content)
 
         else:  # if this is a directory
-            pass
+            if os.path.isdir(name):
+                if len(hosts) == 0:  # A directory only on this server
+                    shutil.rmtree(f"{settings.WORK_DIR}{name}")
+            else:
+                os.mkdir(f"{settings.WORK_DIR}{name}")
 
 
 def registry(host_naming, host_ip, host_port):
@@ -40,15 +46,21 @@ def registry(host_naming, host_ip, host_port):
         f"{host_naming}/api/server/register/",
         params={"space": free_space, "host": host_ip, "port": host_port},
     )
-    print(f"Response from naming server: {r.status_code}")
+    logger.info(f"Response from naming server: {r.status_code}")
 
-    print(json.loads(r.text).get("files", []))
+    recovery(json.loads(r.text).get("files", []))
+
+    _, _, free_space = shutil.disk_usage("/")
+    r = requests.get(
+        f"{host_naming}/api/server/approve/",
+        params={"space": free_space, "host": host_ip, "port": host_port},
+    )
 
 
 def create_workdir(workdir):
     try:
         os.mkdir(workdir)
     except OSError:
-        print("Working directory already exists")
+        logger.info("Working directory already exists")
     else:
-        print("Successfully created the working directory")
+        logger.info("Successfully created the working directory")
